0d7c68f3fc90c741a1bd4c94c5752f66f97f25b4
getsoss <getsoss@naver.com>
2024-05-14 15:01:59 +0900

    fix app.jsx

--- App.jsx
+++ App.jsx
@@ -1,18 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import styled from 'styled-components';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Container from './components/Container';
-import Button from './components/Button';
-import '/Users/soss/Desktop/cross/cross-app/css/common.css';
+import '/css/common.css';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Header></Header>
       <Container>
-        
       </Container>
       <Footer></Footer>
     </View>
