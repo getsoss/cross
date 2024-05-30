1de76b919a31325e2317208b8c1930855e1d8ba7
rogi-rogi <yrkim6883@gmail.com>
2024-05-30 20:37:10 +0900

    App.jsx 실험기능 해제

--- App.jsx
+++ App.jsx
@@ -5,25 +5,24 @@ import Container from "./src/components/Container";
 import { useEffect } from "react";
 import getSubwayPath from "./utils/api/getSubwayPath";
 import { findTopMeetingPoint } from "./utils/findMeetingPoint.js";
+import SubwayInput from "./src/components/SubwayInput.jsx";
 
 export default function App() {
-  useEffect(async () => {
-    console.log("[경로 가져오기]===========================");
-    const station_list = await getSubwayPath(["안양", "기흥", "", "1"]);
-    station_list.map((station) =>
-      console.log(`${station.startStationName} --> ${station.endStationName}`)
-    );
+  // useEffect(async () => {
+  //   console.log("[경로 가져오기]===========================");
+  //   const station_list = await getSubwayPath(["안양", "기흥", "", "1"]);
+  //   station_list.map((station) =>
+  //     console.log(`${station.startStationName} --> ${station.endStationName}`)
+  //   );
 
-    console.log("[중간지점 계산]==========================");
-    const start_station_list = ["복정", "고색", "홍대입구"];
-    findTopMeetingPoint(start_station_list, 0.5, 10);
-    console.log("=========================================");
-  }, []);
+  //   console.log("[중간지점 계산]==========================");
+  //   const start_station_list = ["복정", "고색", "홍대입구"];
+  //   findTopMeetingPoint(start_station_list, 0.5, 10);
+  //   console.log("=========================================");
+  // }, []);
   return (
     <View style={styles.container}>
-      <Header></Header>
-      <Container></Container>
-      <Footer></Footer>
+      <SubwayInput />
     </View>
   );
 }
