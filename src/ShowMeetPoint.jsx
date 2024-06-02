b727f2f9dabf7ecebcf2cbdab8ecdf9577c8bd5f
rogi-rogi <yrkim6883@gmail.com>
2024-06-02 22:24:20 +0900

    출발지 없는 경우 에러 문구 표시

--- src/ShowMeetPoint.jsx
+++ src/ShowMeetPoint.jsx
@@ -1,14 +1,26 @@
 import { useEffect } from "react";
-import { Text, View } from "react-native";
+import { Image, Text, View } from "react-native";
 import ShowMeetPointStyles from "../style/ShowMeetPoint/ShowMeetPointStyles";
-
+import { MaterialCommunityIcons } from "@expo/vector-icons";
 export default ShowMeetPoint = ({ locationList }) => {
   useEffect(() => {}, []);
   return (
     <View style={ShowMeetPointStyles.container}>
       {locationList !== undefined &&
         locationList.map((place, idx) => <Text key={idx}>{idx + place}</Text>)}
-      {locationList === undefined && <Text>검색 결과가 없습니다😥</Text>}
+      {locationList === undefined && (
+        <View style={ShowMeetPointStyles.errorContainer}>
+          <MaterialCommunityIcons
+            name="map-marker-off"
+            size={24}
+            color="black"
+            style={ShowMeetPointStyles.errorBackground}
+          />
+          <Text style={ShowMeetPointStyles.errorMsg}>
+            검색 결과가 없습니다😥
+          </Text>
+        </View>
+      )}
     </View>
   );
 };
--- style/ShowMeetPoint/ShowMeetPointStyles.jsx
+++ style/ShowMeetPoint/ShowMeetPointStyles.jsx
@@ -3,6 +3,21 @@ import { StyleSheet } from "react-native";
 export default ShowMeetPointStyles = StyleSheet.create({
   container: {
     flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "white",
   },
-  errorMsg: {},
+  errorContainer: {
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  errorBackground: {
+    fontSize: 300,
+    opacity: 0.5,
+  },
+  errorMsg: {
+    bottom: 10,
+    fontSize: 40,
+    backgroundColor: "white",
+  },
 });
